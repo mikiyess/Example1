33226f0ce7b160c163402ab15aab7de6b8a0b757
felixbvf <felixbvf@gmail.com>
2017-01-12 17:57:48 -0400

    add view

---
+++ resources/views/index1.blade.php
@@ -0,0 +1,10 @@
+<html lang="es">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>PRUEBA DE MICKY</title>
+    <link rel="stylesheet" href="css/style.css" media="all" />
+  </head>
+  <body>
+            <h1>HOLA MICKY VISUALIZANDO UNA VISTA</h1>
+</body>
+</htm>
